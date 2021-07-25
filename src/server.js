7e442abac4de97b04a074b74d20ab935968e7f59
chuksys <chuksagbakuru@gmail.com>
2021-07-25 12:30:06 +0100

    Setup Server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const { createServer } = require('http');
+
+async function server(app) {
+  const port = Number(process.env.PORT || '3001');
+  app.set('port', port);
+
+  const server = createServer(app);
+  server.listen(port);
+
+  server.on('error', onError);
+
+  server.on('listening', () => {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+      ? `pipe ${addr}`
+      : `port ${addr.port}`;
+    
+    console.log(`Listening on ${bind}`);
+  });
+
+  return server;
+}
+
+function onError(error) {
+  console.error(`Failed to start server:\n${error.stack}`);
+  process.exit(1);
+}
+
+module.exports = server;
